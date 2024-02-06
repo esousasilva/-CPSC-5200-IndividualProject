db78d99972e855e7ebea618d12daf18d5e9f6531
Eduardo Sousa Silva <esousasilva@seattleu.edu>
2024-02-05 23:34:35 -0800

    Changed the name of 2nd button

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -57,7 +57,7 @@
             button2.Name = "button2";
             button2.Size = new Size(206, 81);
             button2.TabIndex = 1;
-            button2.Text = "Gray";
+            button2.Text = "Do it!";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
